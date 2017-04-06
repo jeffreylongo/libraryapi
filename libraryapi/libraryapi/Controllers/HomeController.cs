ffb5f9800f4980e880a0049f87115f3422b53a74
Jeff Longo <jeffreylongo@yahoo.com>
2017-04-06 14:14:29 -0400

    put in gets puts posts and deletes.

--- libraryapi/libraryapi/Controllers/HomeController.cs
+++ libraryapi/libraryapi/Controllers/HomeController.cs
@@ -1,18 +1,66 @@
-﻿using System;
+﻿using libraryapi.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using System.Web.Mvc;
+using System.Web.Http;
 
 namespace libraryapi.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : ApiController
     {
-        public ActionResult Index()
+        public static List<Books> Books { get; set; } = new List<Books>
+        {
+            new Books {Title = "The Gunslinger", Author = "Stephen King", YearPublished = 1982, Genre = "Horror", IsCheckedOut = false, LastCheckedOutDate = 0, DueBackDate = 0 }
+        };
+
+        [HttpGet]
+        public IEnumerable<Books> GetAllBooks()
+        {
+            return Books;
+        }
+
+        [HttpGet]
+        public IHttpActionResult GetBook(string title)
+        {
+            return Ok(Books.FirstOrDefault(f => String.Compare(title, f.Title) == 0));
+        }
+
+        [HttpPut]
+        public Books PutBook(string title, string author)
+        {
+            var b = new Books { Title = title, Author = author };
+            Books.Add(b);
+            return b;
+        }
+
+        [HttpPost]
+        public IHttpActionResult PostBook(Books updated)
+        {
+            var found = Books.FirstOrDefault(f => f.Id == updated.Id);
+            if (found == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                found.Title = updated.Title;
+                return Ok(found);
+            }
+        }
+
+        [HttpDelete]
+        public IHttpActionResult DeleteBook(Guid id)
+        {
+            Books = Books.Where(w => w.Id != id).ToList();
+            return Ok();
+        }
+
+        /*public ActionResult Index()
         {
             ViewBag.Title = "Home Page";
 
             return View();
-        }
+        }*/
     }
 }
--- libraryapi/libraryapi/Models/Books.cs
+++ libraryapi/libraryapi/Models/Books.cs
@@ -8,11 +8,12 @@ namespace libraryapi.Models
     public class Books
     {
         public Guid Id { get; set; } = Guid.NewGuid();
+        public int ID { get; set; }
         public string Title { get; set; }
         public string Author { get; set; }
         public int YearPublished { get; set; }
         public string Genre { get; set; }
-        public bool IsCheckedOutDate { get; set; }
+        public bool IsCheckedOut { get; set; }
         public int LastCheckedOutDate { get; set; }
         public int DueBackDate { get; set; }
     }
